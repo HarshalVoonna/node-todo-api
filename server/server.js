4d8d4759d06f45699ad6f7c6b09e1d2ccaa7398d
Shreeharsha Nageshwar Voonna <shreeharsha.nageshwarvoonna@citrix.com>
2017-10-09 23:30:26 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo.js');
@@ -32,6 +33,27 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+      return res.status(404).send({
+        'error': 'Invalid Id'
+      });
+    }
+    Todo.findById(id).then((todo)=> {
+      if(!todo){
+        return res.status(404).send({
+          'error': 'No TODO found'
+        });
+      }
+      return res.status(200).send({todo})
+    }).catch((e) => {
+      res.status(400).send({
+        'error': 'Invalid request'
+      });
+    });
 });
 
 app.listen(3000, () => {
