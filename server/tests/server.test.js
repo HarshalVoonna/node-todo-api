c745c881d34622eddc4086179123bffb0ed1a262
Shreeharsha Nageshwar Voonna <shreeharsha.nageshwarvoonna@citrix.com>
2017-10-12 00:35:33 +0530

    Add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -11,7 +11,9 @@ const todos = [{
   text: 'First test todo'
 },{
   _id: new ObjectID(),
-  text: 'Second test todo'
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 123
 }];
 
 beforeEach((done)=> {
@@ -140,4 +142,33 @@ describe('DELETE /todos/:id', () => {
       })
       .end(done);
   })
+});
+
+describe('PATCH /todos/:id', () => {
+  it('should update the todo',(done) => {
+    var text = 'Updated text';
+    request(app)
+      .patch(`/todos/${todos[0]._id.toHexString()}`)
+      .send({text, completed: true})
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true);
+        // expect(res.body.todo.completedAt).toBeA('number');
+      })
+      .end(done);
+  });
+  it('should clear completedAt when todo is not completed',(done) => {
+    var text = 'Updated text';
+    request(app)
+      .patch(`/todos/${todos[1]._id.toHexString()}`)
+      .send({text, completed: false})
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(false);
+        expect(res.body.todo.completedAt).toBeNull();
+      })
+      .end(done);
+  });
 });
