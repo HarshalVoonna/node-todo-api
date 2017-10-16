385572d83451c9be60fb083b2ac3b09339ed7e04
Shreeharsha Nageshwar Voonna <shreeharsha.nageshwarvoonna@citrix.com>
2017-10-16 21:50:26 +0530

    Hashing user passwords

--- playground/hashing-jwt.js
+++ playground/hashing-jwt.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 var data = {
   id: 10
@@ -9,3 +10,15 @@ console.log(token);
 
 var decoded = jwt.verify(token, 'somesecretsalt');
 console.log('decoded',decoded);
+
+var password = '123abc!';
+var salt = bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
+
+var hashedPassword = '$2a$10$4Hxv3LzFq22JFXkUt7fo0.XxbyVaV3ACNNzhKE2mRiQOOaeXyXOrS';
+bcrypt.compare(password, hashedPassword, (err, result) => {
+  console.log(result);
+});
