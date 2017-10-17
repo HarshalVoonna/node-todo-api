56796d4c5ff70831cba062e3f5cbbd7baf002655
Shreeharsha Nageshwar Voonna <shreeharsha.nageshwarvoonna@citrix.com>
2017-10-17 23:36:29 +0530

    Updating test methods with latest expect package

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -171,7 +171,7 @@ describe('PATCH /todos/:id', () => {
       .expect((res) => {
         expect(res.body.todo.text).toBe(text);
         expect(res.body.todo.completed).toBe(true);
-        // expect(res.body.todo.completedAt).toBeA('number');
+        expect(typeof res.body.todo.completedAt).toBe('number');
       })
       .end(done);
   });
@@ -199,7 +199,7 @@ describe('PATCH /todos/:id', () => {
       .expect((res) => {
         expect(res.body.todo.text).toBe(text);
         expect(res.body.todo.completed).toBe(false);
-        expect(res.body.todo.completedAt).toNotExist();
+        expect(res.body.todo.completedAt).toBeFalsy();
       })
       .end(done);
   });
@@ -238,8 +238,8 @@ describe('POST /users', () => {
       .send({email, password})
       .expect(200)
       .expect((res) => {
-        expect(res.headers['x-auth']).toExist();
-        expect(res.body._id).toExist();
+        expect(res.headers['x-auth']).toBeTruthy();
+        expect(res.body._id).toBeTruthy();
         expect(res.body.email).toBe(email);
       })
       .end((err) => {
@@ -247,8 +247,8 @@ describe('POST /users', () => {
           return done(err);
         }
         Users.findOne({email}).then((user) => {
-          expect(user).toExist();
-          expect(user.password).toNotBe(password);
+          expect(user).toBeTruthy();
+          expect(user.password).not.toBe(password);
           done();
         }).catch((e) => done(e));
       });
@@ -287,14 +287,14 @@ describe('POST /users/login', () => {
         })
         .expect(200)
         .expect((res) => {
-          expect(res.header['x-auth']).toExist();
+          expect(res.header['x-auth']).toBeTruthy();
         })
         .end((err, res) => {
           if(err){
             return done(err);
           }
           Users.findById(users[1]._id).then((user)=> {
-            expect(user.tokens[1]).toInclude({
+            expect(user.toObject().tokens[1]).toMatchObject({
               access: 'auth',
               token: res.header['x-auth']
             });
@@ -312,7 +312,7 @@ describe('POST /users/login', () => {
       })
       .expect(400)
       .expect((res) => {
-        expect(res.header['x-auth']).toNotExist();
+        expect(res.header['x-auth']).toBeFalsy();
         expect(res.body.errorMessage).toBe('Incorrect password');
       })
       .end((err, res) => {
